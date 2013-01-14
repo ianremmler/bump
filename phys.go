7d1cd88b963f38070449bbc0f3df871d6da1bd42
Ian Remmler <ian@remmler.org>
2013-01-14 10:03:09 -0600

    Woo, bouncing balls!

--- phys.go
+++ phys.go
@@ -4,12 +4,13 @@ import (
 	"github.com/ftrvxmtrx/gochipmunk/chipmunk"
 	"github.com/ianremmler/gordian"
 
+// 	"fmt"
 	"math/rand"
 	"time"
 )
 
 const (
-	simTime    = time.Second / 60
+	simTime    = time.Second / 100
 	updateTime = time.Second / 20
 	size       = 500.0
 )
@@ -28,7 +29,7 @@ type sim struct {
 
 func newSim() *sim {
 	s := &sim{}
-	s.ballBodies = make([]chipmunk.Body, 20)
+	s.ballBodies = make([]chipmunk.Body, 50)
 	s.ballShapes = make([]chipmunk.Shape, len(s.ballBodies))
 	s.box = make([]chipmunk.Shape, 4)
 	gravity := chipmunk.Vect{0, -100}
@@ -42,8 +43,6 @@ func newSim() *sim {
 		s.space.AddBody(s.ballBodies[i])
 		s.ballShapes[i] = chipmunk.CircleShapeNew(s.ballBodies[i], radius,
 			chipmunk.Vect{0, 0})
-		s.ballBodies[i].SetPosition(chipmunk.Vect{radius + rand.Float64()*(size-2*radius),
-			0.5*size + rand.Float64()*(0.5*size-radius)})
 		s.space.AddShape(s.ballShapes[i])
 		s.ballShapes[i].SetFriction(0.1)
 		s.ballShapes[i].SetElasticity(0.9)
@@ -62,6 +61,7 @@ func newSim() *sim {
 		s.box[i].SetFriction(1.0)
 		s.space.AddShape(s.box[i])
 	}
+	s.dropBalls()
 	return s
 }
 
@@ -70,6 +70,14 @@ func (s *sim) cleanup() {
 	// 	s.ballBodies.Free()
 	// 	s.ballShape.Free()
 	// 	s.box.Free()
+}
+
+func (s *sim) dropBalls() {
+	for i := range s.ballBodies {
+		radius := s.ballShapes[i].(chipmunk.CircleShape).Radius()
+		s.ballBodies[i].SetPosition(chipmunk.Vect{radius + rand.Float64()*(size-2*radius),
+			0.5*size + rand.Float64()*(0.5*size-radius)})
+	}
 }
 
 type Phys struct {
@@ -110,15 +118,19 @@ func (p *Phys) run() {
 				client.Id = p.curId
 				client.Ctrl = gordian.REGISTER
 				p.clients[client.Id] = struct{}{}
+				p.sim.dropBalls()
 				p.Control <- client
 			case gordian.CLOSE:
 				delete(p.clients, client.Id)
 			}
 		case msg = <-p.InMessage:
 			data = msg.Data.(map[string]interface{})
-			idx := int(data["data"].(float64)) - 1
-			impulse := chipmunk.Vect{200*rand.Float64() - 100, 200*rand.Float64() - 100}
-			p.sim.ballBodies[idx].ApplyImpulse(impulse, chipmunk.Vect{0, 0})
+			a := data["data"].([]interface{})
+			idx := int(a[0].(float64)) - 1
+			if idx > 0 {
+				impulse := chipmunk.Vect{1000*rand.Float64() - 500, 1000*rand.Float64() - 500}
+				p.sim.ballBodies[idx].ApplyImpulse(impulse, chipmunk.Vect{0, 0})
+			}
 		case <-p.simTimer:
 			p.sim.space.Step(float64(simTime) / float64(time.Second))
 		case <-p.updateTimer:
